8a05c6df6475f5ce6039bb60418b485177b2d619
shinokk <jbourgouin3@gmail.com>
2022-03-09 11:59:34 +0100

    Update donnees 09/03

--- src/Donnees/Sauvegarde.rb
+++ src/Donnees/Sauvegarde.rb
@@ -2,22 +2,29 @@
 # Une Sauvegarde permet de sauvegarder l'historique des coups de l'utilisateur sur une grille
 # et de sauvegarder ou de récupérer les informations sur l'utilisateur 
 #
+require 'fileutils'
 
 class Sauvegarde
 
     ##
     # @nom => nom de l'utilisateur a charger ou sauvegarder
     
-
     def initialize(unNom)
-        if File.exist?('../Sauvegarde/unNom.dump') == 1
-            Marshal.load(File.binread('../Sauvegarde/' + unNom + '.dump'))
-        else 
-            Marshal.dump('../Sauvegarde/' + unNom + '.dump')
-        end 
+        if File.exist?('../../Profile/' + unNom + '.dump') == false
+            my_dir = Dir["../../assets/levels"]
+            my_dir.each do |filename| 
+              FileUtils.cp(filename, "C:/Documents and Settings/user/Desktop/destinationfolder/****/" + File.basename(filename, ".doc"))
+            end
     end
 
-    #Sauvegarde les informations sur l'utilisateur
+    ##
+    # [Sauvegarde les informations sur l'utilisateur]
+    #
+    # === Attributes ===
+    #
+    # UnString- Le nom de L'utilisateur 
+    # uneLangue- la langue choisie par l'utilisateur 
+    # nbEtoiles- le nombre d'etoiles que l'utilisateur 
     def sauvInfosUtilisateur(unString, uneLangue, nbEtoiles)
         file = File.open(chemin, "r+")
         file.write(unString + "\n" + uneLangue.to_s + "\n" + nbEtoiles.to_s)
