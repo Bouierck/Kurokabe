c25df6e38bc067406568a02feddc4181df571ce0
Ben <benjaminlardais@gmail.com>
2022-03-09 12:01:27 +0100

    Petite modif boutonMenu

--- src/Boutons/BoutonMenu.rb
+++ src/Boutons/BoutonMenu.rb
@@ -1,32 +1,35 @@
 #!/usr/bin/env ruby
 
 require 'gtk3'
+require_relative "/home/user/code/somelogic.rbMenus"
 Gtk.init
 
 class BoutonMenu
     private_class_method :new
 
-    window = Gtk::Window.new
-    window.signal_connect('destroy') {
-        Gtk.main_quit
-    }
 
-
-    def initialize(unMenu)
+    def initialize(unMenu, foncMenu)
         @menu = unMenu
+        @fonctionMenu = foncMenu
     end
 
     def menu()
         return @menu
     end
 
-    def BoutonMenu.creer()
+    def fonctionMenu()
+        fonctionMenu = @
+
+    end
+    def BoutonMenu.creer(fonctionMenu)
         new()
     end
 
+
+
     def lanceMenu()
         monBoutonMenu.signal_connect('clicked') {
-            print"Bonsoir"
+            
         }
     
     end
@@ -38,15 +41,13 @@ class BoutonMenu
 
     
     monBoutonMenu = Gtk::Button.new("Menu 1")
-    monBoutonMenu.active=true
-    hb.pack_start(monBoutonMenu)
 
     window = Gtk::Window.new
     window.signal_connect('destroy') {
         Gtk.main_quit
     }
 
-    window.add(hb)
+    window.add(monBoutonMenu)
 
     window.show_all
     Gtk.main
