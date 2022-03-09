92493902e76bf23b68be8f43c80c3aaf9f13444b
ClemStun <stundeveloppe@gmail.com>
2022-03-09 10:59:12 +0100

    pour merge

--- src/Historique.rb
+++ src/Historique.rb
@@ -8,16 +8,46 @@ class Historique
         @listeCoups = Array.new()
         @index = 0
     end
-
+   
+    ##
+    # Ajoute un nouveau coup dans l'historique
+    # Le retour en avant n'est plus possible après un nouveau coup !
+    #
     def nouveauCoup(x, y):
+
+        if(@index + 1 < @listeCoups.size)
+            @listeCoups = @listeCoups.take(@index + 1)
+        end
+ 
+        @index++
+        @listeCoups.push([x, y])
+
+        return
+
     end
     
+    ##
+    # Retourne sur le mouvement précédent
+    #
     def retourArriere:
-        @index -= 1
+        if(@index > 0)
+            @index -= 1
+            
+            #TODO
+            #Effectuer mouvement inverse
+        end
     end
 
+    ##
+    # Retourne sur le mouvement précédent
+    #
     def retourAvant:
-        @index += 1
+        if(@index < @listeCoups.size - 1)
+            @index += 1
+
+            #TODO
+            #Effectuer mouvement
+        end
     end
 
 end
