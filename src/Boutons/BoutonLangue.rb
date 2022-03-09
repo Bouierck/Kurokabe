6793ef5d45f74d78f0491c6f34e6ab69cc41a4f5
eAxium <s184247@ic2s112-01.univ-lemans.fr>
2022-03-09 11:56:18 +0100

    Modification Bouton Langue

--- src/Boutons/BoutonLangue.rb
+++ src/Boutons/BoutonLangue.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/env ruby
 
 require 'gtk3'
+Gtk.init
 
-class BoutonLangue implements Bouton
+class BoutonLangue
 
     private_class_method :new
 
@@ -25,17 +26,18 @@ class BoutonLangue implements Bouton
     def onDestroy
         puts "Fin de l'appli"
         Gtk.main_quit
-    end
-    
-    monBoutonLangue = Gtk::Button.new
-    
-    monBoutonLangue.add(box)
-    monBoutonLangue.show_all
-    
-    #fenetre detruite = quitter
-    monBoutonLangue.signal_connect('destroy') {onDestroy}
+    end  
 
+    fenetre = Gtk::Window.new
+    fenetre.signal_connect('destroy') {
+        Gtk.main_quit
+    }
+
+    monBoutonLangue = Gtk::Button.new("Changement de la langue")
+
+    fenetre.add(monBoutonLangue)
+
+    fenetre.show_all
     Gtk.main
-
 
 end
