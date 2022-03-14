7866a429bd7385c53ea3400fdfc4354ca40a1313
Hugo COUTANT <hugo12001111@gmail.com>
2022-03-14 10:44:44 +0100

    initialize grille

--- src/Niveau/Grille.rb
+++ src/Niveau/Grille.rb
@@ -1,28 +1,40 @@
 class Grille
 
-    def Grille.creer(utilisateur, hauteur, largeur)
-        new(utilisateur, hauteur, largeur)
+    def Grille.creer(utilisateur, nomGrille)
+        new(utilisateur, nomGrille)
     end
 
     private_class_method :new
 
-    def initialize(utilisateur, hauteur, largeur)
+    def initialize(utilisateur, nomGrille)
         @utilisateur = utilisateur
-        @matrice = Array.new(hauteur) { Array.new(largeur) { 0 } }
+
+        chemin = "../../profile/" + @utilisateur.nom + "/" + nomGrille
+        fichierMap = File.open(chemin)
+        tailleGrilleX = fichierMap.read.split("\n")[0]
+        tailleGrilleY = fichierMap.read.split("\n")[1]
+        donneesCases = fichierMap.read.split("\n")[2]
+        donneesHistorique = fichierMap.read.split("\n")[3]
+        
+        @matrice = Array.new(tailleGrilleY) { Array.new(tailleGrilleX) { 0 } }
+
+        int x, y = 0, 0
+        for chiffre in donneesCases do
+            if chiffre == -1
+                @matrice[y][x] = (CaseCliquable.creer(tailleGrilleX, tailleGrilleY))
+            else
+                @matrice[y][x] = (CaseChiffre.creer())
+            end
+            x = x+1%TailleGrilleX
+            y += 1 if x == 0
+        end
+
+        for histo in donneesHistorique do
+
+        end
     end
 
     attr_reader :matrice
-
-    def grilleInitilise(nomGrille)
-        chemin = "../profile/" + @utilisateur.nom + "/" + nomGrille
-        fichierMap = File.open(chemin)
-        donneesCases = fichierMap.read.split("|")[0]
-        donneesHistorique = fichierMap.read.split("|")[1]
-        
-        for chiffre in donneesCases do
-            
-        end
-    end
 
     def grilleAfficher
 
