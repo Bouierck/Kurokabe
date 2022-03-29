d216e7294b5e442f6acbdaaa4702dc37eb7533b4
oussamablk-1 <78028622+oussamablk-1@users.noreply.github.com>
2022-03-29 16:32:31 +0200

    Changement de niveaux

--- src/Niveau/Niveau.rb
+++ src/Niveau/Niveau.rb
@@ -3,71 +3,131 @@
 # Date : Vendredi 25/02 2022 11:36
 #
 require 'gtk3'
+require_relative '../Menus/Menu.rb'
+require_relative '../Boutons/BoutonSpecial.rb'
+require_relative '../Boutons/BoutonPause.rb'
+require_relative '../Boutons/BoutonNiveau.rb'
+require_relative '../Boutons/BoutonMenu.rb'
+require_relative './Grille.rb'
+require_relative './Chronometre.rb'
 
-class Niveau
+class Niveau < Gtk::Builder
+
 
     ##
     #@idNiveau => Numero du niveau 
-    #@grille => Bouton pour accéder a la grille
     #@tailleFenetre => est de type boolean pour savoir la la fenetre est en taille reduite ou en grande taille (false -> petite fenetre) ( true -> grande fenetre)
-    #@Chrono => ce type int qui correspond au chronometre du niveau
-
-    def initialize(idNiveau,uneGrille)
+    def initialize(idNiveau, unUtilisateur, unMode)
+        @mode =unMode
+        @utilisateur = unUtilisateur
         @idNiveau = idNiveau
-        @grille = uneGrille
         @tailleFenetre = false
+
+        #@resolveur = new Resolveur()
+        #@menuPrincipal = new MenuPrincipal()
+        #@menuPause = new MenuPause()
+        @grille = Grille.creer(@utilisateur,"level" + @idNiveau.to_s, @mode)
+        @chrono = Chronometre.creerChrono() 	
     end
+
+    attr_reader :chrono, :grille
 
     ##
     #Methode de creation de niveau
-    def Niveau.Creer()
-        new(idNiveau,uneGrille)
+    def Niveau.Creer(idNiveau, unUtilisateur, unMode)
+        new(idNiveau,unUtilisateur,unMode)
+    end 
+
+
+
+    ##
+    #Affichage de la fenetre du niveau
+    def NiveauAffiche()
+
+        boutonArriere= BoutonSpecial.creer("retour arriere",1,1,@grille.historique.method(:retourArriere))
+        boutonAvant = BoutonSpecial.creer("retour avant",2,2,@grille.historique.method(:retourAvant))
+        boutonPause = BoutonPause.creer("Pause",2,10,Menu.new,self)
+        #boutonReinitialiser = BoutonSpecial.creer("reinitialiser",2,2,@grille)
+        #boutonNiveau = BoutonNiveau.creer("niveau",2,10,@idNiveau)
+        #boutonCheck = BoutonSpecial.creer("check",2,2,resolveur.resoudreGrille())
+        #boutonIndice = BoutonSpecial.creer("indice",2,2,@grille.method(:compareGrille))
+        boutonQuitter = BoutonSpecial.creer("quitter",2,20,self.method(:QuitterFenetre))
+        #boutonMenu = BoutonMenu.creer("Menu",2,10,menuPrincipal.show_all)
+        
+
+        #Creation de la fenetre 
+        window = Gtk::Window.new
+        #window.set_default_size(650,700)
+        window.set_border_width(5)
+        window.set_window_position(Gtk::WindowPosition::CENTER_ALWAYS)
+
+        #Creation header
+        header = Gtk::HeaderBar.new
+        header.show_close_button = true
+        header.name = "headerbar"
+        header.title = "Kurikabe"
+        header.subtitle = " "
+        window.titlebar = header
+
+        #Creation du container pour tous les boutons (important pour contriler la taille ou encore ou ils sont positionné sur l'application)
+        box = Gtk::Box.new(:horizontal,2)
+        box.can_focus = false
+        box.set_homogeneous(true)
+        box.set_spacing(3)
+
+        @grille.set_margin_left(50)
+        @grille.set_margin_right(50)
+        @grille.set_margin_top(59)
+        @grille.set_margin_bottom(80)
+        box.add(@grille)
+
+        boxDroite = Gtk::Box.new(:vertical,7)
+        boxDroite.set_margin_left(50)
+        boxDroite.set_margin_right(50)
+        boxDroite.set_margin_top(15)
+        boxDroite.set_margin_bottom(15)
+        boxDroite.set_homogeneous(true)
+        boxDroite.set_spacing(23)
+        boxDroite.set_homogeneous(true)
+
+        nomNiveau=Gtk::Label.new()
+        nomNiveau.set_markup("Niveau #{@idNiveau}")
+        nomNiveau.set_justify(:center)
+
+        boxDroite.add(nomNiveau)
+        boxDroite.add(@chrono.lancer)
+        boxDroite.add(boutonPause)
+        #boxDroite.add(boutonNiveau)
+
+        #Ajout des boutons d'aide
+        boxAide = Gtk::Box.new(:horizontal,5)
+        boxAide.set_homogeneous(false)
+
+        boxAide.add(boutonArriere)
+        boxAide.add(boutonAvant)
+        #boxAide.add(boutonCheck)
+        #boxAide.add(boutonReinitialiser)
+        #boxAide.add(boutonIndice)
+        
+        boxDroite.add(boxAide)
+        #boxDroite.add(boutonMenu)
+        boxDroite.add(boutonQuitter)
+        
+        box.add(boxDroite)
+
+        #Affichage de la fenetre 
+        window.add(box)
+        window.show_all
+        Gtk.main
     end
 
-
-    ###################
-    #retour niveau, menu, pause(reprendre, niveau, menu, quitter), quitter
-    ###################
-    ###########
-    #Positionner les BoutonSpecial
-    ###########
-
-
-
-    def AfficherBoutton()
-        if (tailleFenetre == false ){
-            BoutonSpecial.AfficheBoutonArriere()
-            BoutonSpecial.AfficheBoutonAvant()
-            BoutonSpecial.AfficheBoutonAide()
-        }
-        
-        BoutonPause.application.show_all
-        BoutonMenu.application.show_all
+    def QuitterFenetre()
+        window.destroy
     end 
 
 
-    def LancerBouton()
-
-        BoutonSpecial boutonArriere= BoutonSpecial.creer(Historique.RetourArriere())
-        BoutonSpecial boutonAvant = BoutonSpecial.creer(Historique.RetourAvant())
-        
-    end 
-
-    #changement de la représentation en fonction de la taille de la fenetre 
-    def FenetreTaille()
-        
-    end 
-
-    def lanceToi(){
-
-        BoutonSpecial boutonArriere= BoutonSpecial.creer(Historique.RetourArriere())
-
-        if FenetreTaille == true
-            Grille.grilleAfficher()
-            AfficherBoutton()
-            LancerBouton()
-        else 
-            Grille.grilleAfficher()
-        end 
-    }
 end # Marqueur de fin de classe
+
+niveau = Niveau.Creer(1,Utilisateur.creer("Stun",1),"aventure")
+niveau.NiveauAffiche()
+Gtk.main
