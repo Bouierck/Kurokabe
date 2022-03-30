274b717d9ef06ed782b3d29a801215bb3ae8c435
Hugo COUTANT <hugo12001111@gmail.com>
2022-03-30 12:00:59 +0200

    tech island exp hugo 30/03

--- src/Aide/Techniques/IslandExp.rb
+++ src/Aide/Techniques/IslandExp.rb
@@ -12,15 +12,24 @@ class IslandExp < Technique
     end 
 
     def verifieTech
-
-        @casesChange = []
-
+        #On parcourt toutes les cases de la grille
         @grille.matrice.each do |line|
             line.each do |c|
+
+                @casesChange = []
+
                 if(c.is_a?(CaseChiffre) && c.chiffre > 1)
-                    
+                    @casesChange << @grille.matrice[c.y][c.x - 1] if ((c.x - 1 >= 0) && @grille.matrice[c.y][c.x - 1].is_a?(CaseCliquable) && @grille.matrice[c.y][c.x - 1].etat == 0)
+                    @casesChange << @grille.matrice[c.y][c.x + 1] if ((c.x + 1 < line.length) && @grille.matrice[c.y][c.x + 1].is_a?(CaseCliquable) && @grille.matrice[c.y][c.x + 1].etat == 0)
+                    @casesChange << @grille.matrice[c.y - 1][c.x] if ((c.y - 1 >= 0) && @grille.matrice[c.y - 1][c.x].is_a?(CaseCliquable) && @grille.matrice[c.y - 1][c.x].etat == 0)
+                    @casesChange << @grille.matrice[c.y + 1][c.x] if ((c.y + 1 < @grille.matrice.length) && @grille.matrice[c.y + 1][c.x].is_a?(CaseCliquable) && @grille.matrice[c.y + 1][c.x].etat == 0)
+
+                    return true if(@casesChange.length == 1)
                 end
             end
         end
+
+        return false
+
     end
 end
